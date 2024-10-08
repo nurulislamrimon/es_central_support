b74e07b7a978a8badf17a9e6103c02abec5ee3ef
nurulislamrimon <nurulislamrimon@gmail.com>
2024-10-08 12:00:16 +0600

    authentication implimented

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,14 +1,26 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../utils/ApiError";
+import { config } from "../config";
 
 export const authentication = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const accessToken = req.headers.authorization?.split(" ")[1];
-  if (!accessToken) {
-    throw new ApiError(403, "Access forbidden!");
+  try {
+    const accessToken = req.headers.authorization?.split(" ")[1];
+    if (!accessToken) {
+      throw new ApiError(403, "Access forbidden!");
+    }
+    //   verify accessToken
+    const payload: JwtPayload | string = jwt.verify(
+      accessToken,
+      config.accessTokenSecret
+    );
+    req.user = payload;
+    next();
+  } catch (error) {
+    next(error);
   }
-  next();
 };
